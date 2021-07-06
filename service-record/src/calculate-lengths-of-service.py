3d657703c4fee0d5c0361cf927be7a2967128d86
SDSIT <sdsitroot@SDSITs-MacBook-Pro.local>
2021-07-06 14:18:08 -0400

    add los calculation code

--- service-record/src/calculate-lengths-of-service.py
+++ service-record/src/calculate-lengths-of-service.py
@@ -2,6 +2,8 @@
 # a program to compute the lengths of service (los) from dsi-individual-service-periods.csv
 # 2020-07-06
 
+import pandas as pd
+
 # read in data file
 
 # clean up and select for start and end date
