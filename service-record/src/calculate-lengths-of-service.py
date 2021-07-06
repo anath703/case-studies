c3a1dcc60897f1943895763d4f9a035a48a4a323
pete <pete.alonzi@gmail.com>
2021-07-06 12:15:11 -0500

    Create calculate-lengths-of-service.py

---
+++ service-record/src/calculate-lengths-of-service.py
@@ -0,0 +1,11 @@
+# lpa2a@virginia.edu
+# a program to compute the lengths of service (los) from dsi-individual-service-periods.csv
+# 2020-07-06
+
+# read in data file
+
+# clean up and select for start and end date
+
+# compute (los)
+
+# write out to file
